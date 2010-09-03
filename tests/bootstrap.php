6f51f1cf0648e05afe9390bf6eb0002d2c6fe054
Matt Light <matt.light@lightdatasys.com>
2010-09-03 03:20:50 +0000

    Updated the unit test bootstrap to use a custom auto-loader.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/trunk@4 7836b164-11d7-4af3-8387-efc7950effad

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,30 @@
 
 
 
+define('PATH_TEST_ROOT', dirname(__FILE__));
+
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
+
+
+
+function __autoload($class_name)
+{
+	$path	= '' . str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+	
+//	var_dump("Loading '{$path}'");
+	
+	require_once $path;
+}
+
+
+
 function bootstrap()
 {
-	require_once 'Zend/Loader/Autoloader.php';
-	$loader	= Zend_Loader_Autoloader::getInstance();
-	$loader->registerNamespace('Mephex_');
-	$loader->registerNamespace('Stub_');
+//	require_once 'Zend/Loader/Autoloader.php';
+//	$loader	= Zend_Loader_Autoloader::getInstance();
+//	$loader->registerNamespace('Mephex_');
+//	$loader->registerNamespace('Stub_');
+	
 }
 
 
