bc8bba0e7fa025d8a83594ace587d23e1cbe1650
Matt Light <matt.light@lightdatasys.com>
2010-10-02 06:08:48 +0000

    Re-factored bootstrap.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/trunk@30 7836b164-11d7-4af3-8387-efc7950effad

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,30 +2,19 @@
 
 
 
-define('PATH_TEST_ROOT', dirname(__FILE__));
-
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__));
-
-
-
-function __autoload($class_name)
-{
-	$path	= '' . str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
-	
-//	var_dump("Loading '{$path}'");
-	
-	require_once $path;
-}
-
-
-
 function bootstrap()
 {
-//	require_once 'Zend/Loader/Autoloader.php';
-//	$loader	= Zend_Loader_Autoloader::getInstance();
-//	$loader->registerNamespace('Mephex_');
-//	$loader->registerNamespace('Stub_');
-	
+	define('PATH_TEST_ROOT', dirname(__FILE__));
+
+	set_include_path(get_include_path() . PATH_SEPARATOR . PATH_TEST_ROOT);
+
+	require_once 'Mephex/App/AutoLoader.php';
+	require_once 'Mephex/App/ClassLoader/PathOriented.php';
+
+	$auto_loader	= Mephex_App_AutoLoader::getInstance();
+	$auto_loader->addClassLoader(new Mephex_App_ClassLoader_PathOriented('Mephex_'));
+	$auto_loader->addClassLoader(new Mephex_App_ClassLoader_PathOriented('Stub_'));
+	$auto_loader->registerSpl();
 }
 
 
