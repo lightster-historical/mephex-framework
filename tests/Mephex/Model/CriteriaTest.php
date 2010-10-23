4681276f02b14c8e50f0ae9a4d0196c24e760903
Matt Light <matt.light@lightdatasys.com>
2010-10-23 05:11:39 +0000

    Updated Mephex_Model_Criteria test (this file was missed during r42 commit).
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/trunk@43 7836b164-11d7-4af3-8387-efc7950effad

--- tests/Mephex/Model/CriteriaTest.php
+++ tests/Mephex/Model/CriteriaTest.php
@@ -20,4 +20,40 @@ extends Mephex_Test_TestCase
 	{
 		$this->assertTrue($this->_criteria instanceof Mephex_Model_Criteria);
 	}
+	
+	
+	
+	public function testGetCriteriaValuesReturnsRequestedValues()
+	{
+		$this->assertTrue(
+			array('a' => 1, 'b' => 2) ===
+			$this->_criteria->getCriteriaValues(array('a', 'b'))
+		);
+		$this->assertFalse(
+			array('a' => 1, 'b' => 2) ===
+			$this->_criteria->getCriteriaValues(array('b', 'a'))
+		);
+		$this->assertTrue(
+			array('b' => 2, 'a' => 1) ===
+			$this->_criteria->getCriteriaValues(array('b', 'a'))
+		);
+		$this->assertTrue(
+			array('c' => 3) ===
+			$this->_criteria->getCriteriaValues(array('c'))
+		);
+		$this->assertTrue(
+			array('c' => 3, 'a' => 1, 'b' => 2) ===
+			$this->_criteria->getCriteriaValues(array('c', 'a', 'b'))
+		);
+	}
+	
+	
+	
+	/**
+	 * @expectedException Mephex_Exception
+	 */
+	public function testAnExceptionIsThrownWhenRetrievingCriteriaValuesForUnavailableFields()
+	{
+		$this->_criteria->getCriteriaValues(array('d'));
+	}
 }  
