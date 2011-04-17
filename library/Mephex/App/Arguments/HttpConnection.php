16ef85eca05e7a5d0164acc590dbe1f2494249eb
Matt Light <matt.light@lightdatasys.com>
2011-04-17 18:00:34 +0000

    Added HttpConnection arguments class for interfacing with the _SERVER super global.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/trunk@76 7836b164-11d7-4af3-8387-efc7950effad

---
+++ library/Mephex/App/Arguments/HttpConnection.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+/**
+ * Provides information related to the connection to the server through HTTP.
+ * 
+ * @author mlight
+ */
+class Mephex_App_Arguments_HttpConnection
+extends Mephex_App_Arguments
+{
+	/**
+	 * @param array $args - a list of argument values to start off with
+	 */
+	public function __construct(array $args = null)
+	{
+		parent::__construct($args ? $args : $_SERVER);
+	}
+	
+	
+	
+	/**
+	 * Checks to see if the request is an XmlHttp/AJAX request.
+	 * 
+	 * @return bool
+	 */
+	public function isXmlHttpRequest()
+	{
+		return strtolower($this->get('HTTP_X_REQUESTED_WITH')) === 'xmlhttprequest';
+	}
+	
+	
+	
+	/**
+	 * Checks to see if the request is a no-cache request (e.g. the page
+	 * was 'hard-refreshed').
+	 * 
+	 * @return bool
+	 */
+	public function isNoCacheRequest()
+	{
+		return (bool)preg_match('/no-cache/', (string)$this->get('HTTP_CACHE_CONTROL'));
+	}
+}
---
+++ tests/Mephex/App/Arguments/HttpConnectionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+
+
+class Mephex_App_Arguments_HttpConnectionTest
+extends Mephex_Test_TestCase
+{
+	protected function setUp()
+	{
+		parent::setUp();
+	}
+	
+	
+	
+	/**
+	 * @covers Mephex_App_Arguments_HttpConnection::__construct
+	 */
+	public function testDefaultArgumentsAreFromServerSuperGlobal()
+	{
+		$args	= new Mephex_App_Arguments_HttpConnection();
+		
+		$match	= true;
+		foreach($_SERVER as $key => $val)
+		{
+			if($args->get($key) !== $val)
+			{
+				$match	= false;
+			}
+		}
+		
+		$this->assertTrue($match);
+	}
+	
+	
+	
+	/**
+	 * @covers Mephex_App_Arguments_HttpConnection::isXmlHttpRequest
+	 */
+	public function testAbleToDetectXmlHttpRequests()
+	{
+		$args	= new Mephex_App_Arguments_HttpConnection(array('HTTP_X_REQUESTED_WITH' => 'XmlHttpRequest'));
+		$this->assertTrue($args->isXmlHttpRequest());
+		
+		$args	= new Mephex_App_Arguments_HttpConnection(array('HTTP_X_REQUESTED_WITH' => 'SomethingElse'));
+		$this->assertFalse($args->isXmlHttpRequest());
+		
+		$args	= new Mephex_App_Arguments_HttpConnection(array());
+		$this->assertFalse($args->isXmlHttpRequest());
+	}
+	
+	
+	
+	/**
+	 * @covers Mephex_App_Arguments_HttpConnection::isNoCacheRequest
+	 */
+	public function testAbleToDetectNoCacheRequests()
+	{
+		$args	= new Mephex_App_Arguments_HttpConnection(array('HTTP_CACHE_CONTROL' => 'no-cache'));
+		$this->assertTrue($args->isNoCacheRequest());
+		
+		$args	= new Mephex_App_Arguments_HttpConnection(array('HTTP_CACHE_CONTROL' => 'noncache'));
+		$this->assertFalse($args->isNoCacheRequest());
+		
+		$args	= new Mephex_App_Arguments_HttpConnection(array());
+		$this->assertFalse($args->isNoCacheRequest());
+	}
+}
