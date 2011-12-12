808a93d343b778c5fa8b7f1d6af574a8a2b7af51
Matt Light <matt.light@lightdatasys.com>
2011-12-12 06:18:52 +0000

    Added PHPdoc to Mephex_Db_CredentialFactory.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/trunk@132 7836b164-11d7-4af3-8387-efc7950effad

--- library/Mephex/Db/CredentialFactory.php
+++ library/Mephex/Db/CredentialFactory.php
@@ -9,5 +9,11 @@
  */
 interface Mephex_Db_CredentialFactory
 {
+	/**
+	 * Generates the credential of the given name.
+	 *
+	 * @param string $name - the name of the credential to generate
+	 * @return Mephex_Db_Sql_Pdo_Credential
+	 */
 	public function getCredential($name);
 }
