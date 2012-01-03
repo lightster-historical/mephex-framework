aa1426879669fdad5d2f96af48d4962de5f374df
Matt Light <matt.light@lightdatasys.com>
2012-01-03 00:45:32 +0000

    Added Sqlite DB quoter.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/branches/2012q1_db_refactor@141 7836b164-11d7-4af3-8387-efc7950effad

---
+++ library/Mephex/Db/Sql/Base/Quoter/Sqlite.php
@@ -0,0 +1,55 @@
+<?php
+
+
+
+/**
+ * A utility class for properly quoting database table names,
+ * column names, and values in Sqlite.
+ * 
+ * @author lightster
+ */
+class Mephex_Db_Sql_Base_Quoter_Sqlite
+extends Mephex_Db_Sql_Quoter
+{
+	/**
+	 * Quotes a table name.
+	 * 
+	 * @param string $table - the table name to be quoted
+	 * @return string
+	 */
+	public function quoteTable($table)
+	{
+		return '"' . str_replace('"', '""', $table) . '"';
+	}
+	
+	
+	
+	/**
+	 * Quotes a column name.
+	 * 
+	 * @param string $column - the column name to be quoted
+	 * @return string
+	 */
+	public function quoteColumn($column)
+	{
+		return '"' . str_replace('"', '""', $column) . '"';
+	}
+	
+	
+	
+	/**
+	 * Quotes a value.
+	 * 
+	 * @param string $value - the value to be quoted
+	 * @return string
+	 */
+	public function quoteValue($value)
+	{
+		if(null === $value)
+			return 'null';
+		else if(is_numeric($value))
+			return $value;
+		else
+			return "'" . str_replace("'", "''", $value) . "'";
+	}
+}
