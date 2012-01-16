db3d9495ef513c72b5c6dc836c916de2d70281c6
Matt Light <matt.light@lightdatasys.com>
2012-01-16 06:11:12 +0000

    Added Sqlite configurable credential details factory.
    git-svn-id: file:///Users/lightster/backup-8.31.2013_20-06-44_litesign/homedir/public_html/svn/mephex/framework/branches/2012q1_db_refactor@159 7836b164-11d7-4af3-8387-efc7950effad

---
+++ library/Mephex/Db/Sql/Pdo/CredentialDetailsFactory/Configurable/Sqlite.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+/**
+ * Generates credential details for a Sqlite connection using a config option set. 
+ * 
+ * @author mlight
+ */
+class Mephex_Db_Sql_Pdo_CredentialDetailsFactory_Configurable_Sqlite
+extends Mephex_Db_Sql_Pdo_CredentialDetailsFactory_Configurable
+{
+	/**
+	 * Get the credential details with the given name.
+	 *
+	 * @param string $name - the name of the credential/connection
+	 * @return Mephex_Db_Sql_Pdo_CredentialDetails
+	 * @see Mephex_Db_Sql_Pdo_CredentialDetailsFactory#getCredentialDetails
+	 */
+	public function getCredentialDetails($name)
+	{
+		$database	= $this->_config->get($this->_group, "{$name}.database");
+		$dsn		= "sqlite:{$database}";
+		$options	= array(
+			PDO::ATTR_TIMEOUT => $this->_config->get($this->_group, "{$name}.timeout", 1.0)
+		);
+		
+		return new Mephex_Db_Sql_Pdo_CredentialDetails($dsn, null, null, $options);
+	}
+
+
+
+	/**
+	 * Generates the quoter for the given credential/connection name.
+	 *
+	 * @param string $name - the name of the credential/connection to generate
+	 *		the quoter for
+	 * @return Mephex_Db_Sql_Quoter
+	 * @see Mephex_Db_Sql_Pdo_CredentialDetailsFactory#getQuoter
+	 */
+	public function getQuoter($name)
+	{
+		return new Mephex_Db_Sql_Base_Quoter_Sqlite();
+	}
+}
